722c0a50d9bca407cd1479aa2ec63f5c8684a165
gaoshi <gaoshi@qcc.com>
2025-11-11 15:24:46 +0800

    run

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/version/Info.java
@@ -0,0 +1,15 @@
+package org.apache.zookeeper.version;
+
+/**
+ * @Author gaoshi
+ * @create 2025/11/11
+ */
+public interface Info {
+    int MAJOR = 1;
+    int MINOR = 0;
+    int MICRO = 0;
+    String QUALIFIER = null;
+    int REVISION = -1;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2020‐10‐15";
+}
